4fa4fa8d736316286cf3ad4a34baf28434a64d89
Stefan0802 <stpa.leonov@mail.ru>
2025-04-25 11:14:17 +0700

    debug LoginRequest

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -16,7 +16,7 @@ class LoginRequest extends FormRequest
     {
         return [
             'email' => 'required|email',
-            'password' => 'required|string|min:6'
+            'password' => 'required|string'
         ];
     }
 
@@ -26,6 +26,7 @@ class LoginRequest extends FormRequest
             'email.request' => 'Неверная почта',
             'email.email' => 'Логин должен быть почтой',
             'password.request' => 'Неверный пароль',
+            'password.string' => 'Пароль должен быть строкой'
         ];
     }
 }
